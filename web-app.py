9f9048ba0adc3682fac762dc75b6bd62781638bd
yardenavr <117725231+yardenavr@users.noreply.github.com>
2023-02-01 10:29:18 +0200

    Update web-app.py

--- web-app.py
+++ web-app.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python
-# coding: utf-8
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(name)
+
 
 @app.route('/')
-def entry_point():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello():
+    return 'Hello, World!'
